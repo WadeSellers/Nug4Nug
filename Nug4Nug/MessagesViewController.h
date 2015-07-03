4b8b3c1e60259b61a86836d16b3e54f6077adc84
Wade Sellers <Waderoni@gmail.com>
2015-07-02 22:42:50 -0600

    added MessagesViewController class, attached tableview delegate and datasource to this

---
+++ Nug4Nug/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  Nug4Nug
+//
+//  Created by Wade Sellers on 7/2/15.
+//  Copyright (c) 2015 WadeSellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessagesViewController : UIViewController
+
+@end
